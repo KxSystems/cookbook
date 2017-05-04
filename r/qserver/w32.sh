24335bd1cb1342965a930991661b2eaf309eb75f
Sergey Vidyuk <sergey@kx.com>
2017-05-04 16:19:44 +0100

    fix windows builds

--- r/qserver/w32.sh
+++ r/qserver/w32.sh
@@ -1,3 +1,3 @@
 R32=`R.exe RHOME`/bin/i386
 export PKG_CFLAGS=-DKXVER=3
-$R32/R.exe CMD SHLIB -o w32/qserver.dll base.c w32/cst.lib  -lws2_32
+$R32/R.exe CMD SHLIB -o w32/qserver.dll base.c w32/c.lib  -lws2_32
--- r/qserver/w64.sh
+++ r/qserver/w64.sh
@@ -1,3 +1,6 @@
 R64=`R.exe RHOME`/bin/x64
 export PKG_CFLAGS=-DKXVER=3
-$R64/R.exe CMD SHLIB -o w64/qserver.dll base.c w64/cst.obj -m64
+$R64/R.exe CMD SHLIB -o w64/qserver.dll base.c w64/c.lib -lws2_32
+
+
+#http://www.mingw.org/wiki/msvc_and_mingw_dlls
