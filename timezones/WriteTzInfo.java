c5d3dc169ccb797ae7f73650f63ecfd0d23779a8
Edward Richards <edwardorichards@gmail.com>
2023-10-06 09:45:28 +0100

    Code to generate tzinfo from Java taking into account historical offset changes

---
+++ timezones/WriteTzInfo.java
@@ -0,0 +1,66 @@
+// Requires Java 17+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.time.ZoneOffset.UTC;
+import static java.util.TimeZone.getTimeZone;
+import static java.util.stream.Collectors.toMap;
+
+public class WriteTzInfo {
+
+    private static final ZonedDateTime startDate = ZonedDateTime.of(1900, 1, 1, 0, 0, 0, 0, UTC);
+    private static final ZonedDateTime endDate = ZonedDateTime.of(2100, 1, 1, 0, 0, 0, 0, UTC);
+
+    private static List<String> generateAdjustmentLines(String zoneName, ZoneId zoneId) {
+        System.out.println(zoneName);
+        List<String> result = new ArrayList<>();
+        ZoneOffset previousOffset = null;
+        ZonedDateTime currentDate = startDate.withZoneSameInstant(zoneId);
+
+        // Repeatedly increment by 1-minute, checking if the offset changes, and recording when this happens
+        while (currentDate.isBefore(endDate)) {
+            if (!currentDate.getOffset().equals(previousOffset)) {
+                previousOffset = currentDate.getOffset();
+                String timeUtcString = currentDate.withZoneSameInstant(UTC).format(DateTimeFormatter.ofPattern("yyyy.MM.dd'D'HH:mm:ss.SSS"));
+                result.add(zoneName + "," + timeUtcString + "," + previousOffset.getTotalSeconds());
+            }
+            currentDate = currentDate.plusMinutes(1);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(new FileWriter("tzinfo.csv"))) {
+            out.println("timezoneID,gmtDateTime,offset");
+
+            // Get all timezone ids by joining ZoneId (new) and TimeZone (legacy)
+            // ZoneId doesn't include three-letter codes such as EST, and will convert these to a format such as -05:00
+            // So we need to preserve the names that TimeZone uses
+            Map<String, ZoneId> zoneIds = ZoneId.getAvailableZoneIds().stream().collect(toMap(Function.identity(), ZoneId::of));
+            Arrays.stream(TimeZone.getAvailableIDs()).forEach(x -> zoneIds.merge(x, getTimeZone(x).toZoneId(), (zid, tz) -> zid));
+
+            List<String> lines = zoneIds.entrySet().parallelStream()
+                    .map(e -> generateAdjustmentLines(e.getKey(), e.getValue()))
+                    .flatMap(Collection::stream)
+                    .sorted()
+                    .toList();
+
+            for (String line : lines) {
+                out.println(line);
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(WriteTzInfo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
